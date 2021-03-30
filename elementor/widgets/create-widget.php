75668bde637ba195542c3291d15e3e19da27b433
Noruzzaman Rubel <noruzzamanrubel@gmail.com>
2021-03-30 18:08:19 +0600

    update

--- elementor/widgets/create-widget.php
+++ elementor/widgets/create-widget.php
@@ -45,28 +45,6 @@ class Test_Title_widget extends Widget_Base
             ]
         );
 
-        $this->add_control(
-            'subtitle',
-            [
-                'label' => __('Sub-title', 'elementor'),
-                'label_block' => true,
-                'type' => Controls_Manager::TEXT,
-                'placeholder' => __('Enter your sub-title', 'elementor'),
-            ]
-        );
-
-        $this->add_control(
-            'link',
-            [
-                'label' => __('Link', 'elementor'),
-                'type' => Controls_Manager::URL,
-                'placeholder' => __('https://your-link.com', 'elementor'),
-                'default' => [
-                    'url' => '',
-                ],
-            ]
-        );
-
         $this->end_controls_section();
     }
 
@@ -74,9 +52,7 @@ class Test_Title_widget extends Widget_Base
     {
 
         $settings = $this->get_settings_for_display();
-        $url = $settings['link']['url'];
-        echo "<a href='$url'><div class='title'>$settings[title]</div> <div class='subtitle'>$settings[subtitle]</div></a>";
-
+        $title = $settings['title'];
     }
 
 
